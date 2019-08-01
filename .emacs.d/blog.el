27fccb183462681cb41964870222daa6bcd8b8e8
Lae <llaechen@gmail.com>
2019-08-01 22:41:54 +1200

    Update blog.

--- .emacs.d/blog.el
+++ .emacs.d/blog.el
@@ -11,14 +11,18 @@
          :time-stamp-file nil
          :html-doctype "html5"
          :html-validation-link nil
-         :html-head
-         "
+         :html-head "
 <style type='text/css'>
-body { width: 760px; max-width: 95%; margin: auto; padding-bottom: 5em; }
+body { width: 760px; max-width: 95%; margin: auto; padding-bottom: 3em; }
 pre.src:hover:before { display: none; }
-pre.src { padding-top: 8pt; }
-pre { box-shadow: none; }
-</style>"
+pre.src { padding-top: 5pt; }
+pre { box-shadow: none; border: none; }
+</style>
+"
+         :html-postamble "
+<a href=\"index.html\">Index</a> |
+<a href=\"https://gitlab.com/lae/blog\">Source</a>
+"
          :publishing-function org-html-publish-to-html)))
 
 (defun publish-blog ()
