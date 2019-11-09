007191e6510421e4dd5b17be979b95be0eaac91f
Lae <llaechen@gmail.com>
2019-11-09 17:03:44 +1300

    Let dedicated left window to sit on top of centered editor left margin if it fits.

--- .emacs.d/centered-layout.el
+++ .emacs.d/centered-layout.el
@@ -42,36 +42,72 @@ Otherwise set right margin to 0, allowing text to flow to far right."
   (and (boundp 'lv-wnd)
        (eq window lv-wnd)))
 
-(defun centered-layout--window-body-preferred-columns (window)
+(defun centered-layout--window-body-preferred-pixels (window)
   "Return preferred body width for WINDOW.
-For special windows like which-key, lv, allow them to use more than
-`centered-layout-columns' but will still center them."
-  (if (or (centered-layout--which-key-window-p window)
-          (centered-layout--lv-window-p window))
-      (/ (car (window-text-pixel-size
+Usually `centered-layout-columns', but for dedicated
+windows (`window-dedicated-p') like which-key, lv (hydra),
+neotree, treemacs etc, return their content width if it is
+greater, as typically they are used as dedicate bottom/side
+windows and have their own layout, limiting their width will
+cause undesired line wrapping/truncation."
+  (let ((pixels (* centered-layout-columns (window-font-width window))))
+    (if (window-dedicated-p window)
+        (max
+         pixels
+         (car (window-text-pixel-size
                window
                nil
                nil
                (- (window-pixel-width window)
                   (window-scroll-bar-width window)
-                  (frame-fringe-width (window-frame window)))))
-         (window-font-width window))
-    centered-layout-columns))
+                  (window-right-divider-width window)
+                  (frame-fringe-width (window-frame window))))))
+      pixels)))
+
+(defun centered-layout--free-columns (window extra-free-pixels)
+  "Computes available columns for layout use for WINDOW.
+Adds EXTRA-FREE-PIXELS into the calculation."
+  (max
+   (ceiling
+    (- (+ (window-pixel-width window) extra-free-pixels)
+       (window-scroll-bar-width window)
+       (window-right-divider-width window)
+       (frame-fringe-width (window-frame window))
+       (centered-layout--window-body-preferred-pixels window))
+    (frame-char-width (window-frame window)))
+   0))
+
+(defun centered-layout--compute-margins (window)
+  "Compute (LEFT-MARGIN . RIGHT_MARGIN) for WINDOW."
+  (let* (
+         ;; `window-left-column' is needed to ensure `window-left'
+         ;; will return a window to the left, otherwise a window above
+         ;; can also be returned.
+         (dediated-window
+          (if (/= 0 (window-left-column window)) (window-left window) nil))
+
+         (dediated-window-pixels
+          (if dediated-window (window-pixel-width dediated-window) 0))
+
+         (dediated-window-columns
+          (if dediated-window (window-total-width dediated-window 'ceiling) 0))
+
+         (margins (centered-layout--free-columns window dediated-window-pixels))
+         (margin-left (- (ceiling margins 2) dediated-window-columns))
+         (margin-right
+          (if centered-layout-apply-right-margin
+              (- margins dediated-window-pixels margin-left)
+            0)))
+
+    `(,(max 0 margin-left) .
+      ,(max 0 margin-right))))
 
 (defun centered-layout--update-window (window)
   "Update margins for WINDOW.
 If FORCE, force update, otherwise only update if window size has
 changed."
-  (set-window-margins
-   window
-   (max 0 (/ (- (window-pixel-width window)
-                (window-scroll-bar-width window)
-                (frame-fringe-width (window-frame window))
-                (* (window-font-width window)
-                   (centered-layout--window-body-preferred-columns window)))
-             (frame-char-width (window-frame window))
-             2))
-   nil))
+  (let ((margins (centered-layout--compute-margins window)))
+    (set-window-margins window (car margins) (cdr margins))))
 
 (defun centered-layout--update-frame (&optional frame)
   "Update margins for FRAME, if nil, update all frames.
