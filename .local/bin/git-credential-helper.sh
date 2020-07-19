673e854cd65da2aeac340abebdd86a0dcaaa94be
Lae Chen <laec@protonmail.ch>
2020-07-19 22:35:59 +1200

    Update Fedora.

--- .local/bin/git-credential-helper.sh
+++ .local/bin/git-credential-helper.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
+set -o nounset
+set -o errexit
+set -o pipefail
+
 if [[ -n "$WSLENV" ]]; then
   /mnt/c/Program\ Files/Git/mingw64/libexec/git-core/git-credential-manager.exe "$@"
-else
+elif [[ -e /usr/libexec/git-core/git-credential-libsecret ]]; then
+  /usr/libexec/git-core/git-credential-libsecret "$@"
+elif [[ -e /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret ]]; then
   /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret "$@"
+else
+  echo "No git credential helper found." 1>&2
+  exit 1
 fi
