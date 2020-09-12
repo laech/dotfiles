c4aed79c3b0f469addce95b3e1b64067683882ad
Lae Chen <laec@protonmail.ch>
2020-09-13 10:34:29 +1200

    Update settings.

---
+++ .windows/config.bat
@@ -0,0 +1,4 @@
+mklink /D C:\Users\Lae\AppData\Roaming\.emacs.d C:\Users\Lae\projects\self\dotfiles\.emacs.d
+mklink /D C:\Users\Lae\AppData\Roaming\Code\User C:\Users\Lae\projects\self\dotfiles\.config\Code\User
+mklink /J C:\Users\Lae\AppData\Roaming\JetBrains\IdeaIC2020.2 C:\Users\Lae\projects\self\dotfiles\.config\JetBrains\idea
+pause
