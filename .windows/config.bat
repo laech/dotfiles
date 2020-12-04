8e9fcc5c020bb7aa0acf8a8fb857f5ffa598a307
Lae Chen <laec@protonmail.ch>
2020-12-04 14:43:52 +1300

    Update settings.

--- .windows/config.bat
+++ .windows/config.bat
@@ -5,7 +5,7 @@ for /d %%p in ("C:\Users\Lae\AppData\Local\Packages\Microsoft.WindowsTerminal_*"
 mklink /D C:\Users\Lae\Documents\PowerShell C:\Users\Lae\projects\self\dotfiles\.windows\powershell
 mklink /D C:\Users\Lae\AppData\Roaming\.emacs.d C:\Users\Lae\projects\self\dotfiles\.emacs.d
 mklink /D C:\Users\Lae\AppData\Roaming\Code\User C:\Users\Lae\projects\self\dotfiles\.config\Code\User
-mklink /J C:\Users\Lae\AppData\Roaming\JetBrains\IdeaIC2020.2 C:\Users\Lae\projects\self\dotfiles\.config\JetBrains\idea
+mklink /J C:\Users\Lae\AppData\Roaming\JetBrains\IdeaIC2020.3 C:\Users\Lae\projects\self\dotfiles\.config\JetBrains\idea
 
 mklink C:\Users\Lae\.gitconfig C:\Users\Lae\projects\self\dotfiles\.windows\.gitconfig
 
