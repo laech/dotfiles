6729f396683a113d1141a7e0484469ebc0df41aa
Lae Chen <laec@protonmail.ch>
2021-03-09 22:36:39 +1300

    Add Git info.

--- .windows/powershell/Microsoft.PowerShell_profile.ps1
+++ .windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -9,3 +9,33 @@ Set-PSReadLineKeyHandler -Key 'ctrl+v' -Function Paste
 Set-PSReadLineKeyHandler -Key 'ctrl+z' -Function Undo
 Set-PSReadLineKeyHandler -Key 'ctrl+shift+z' -Function Redo
 Set-Alias -Name ll -Value ls
+
+function Write-BranchName () {
+  $branch = ""
+  try {
+    $branch = git rev-parse --abbrev-ref HEAD
+    if ($branch -eq "HEAD") {
+      $branch = git rev-parse --short HEAD
+    }
+  } catch {
+    $branch = "(none)"
+  }
+  Write-Host " ($branch)" -ForegroundColor "magenta"
+}
+
+function prompt {
+  $base = ""
+  $path = "$($executionContext.SessionState.Path.CurrentLocation)"
+  $userPrompt = "$('%' * ($nestedPromptLevel + 1)) "
+
+  Write-Host "`n$base" -NoNewline
+
+  if (Test-Path .git) {
+    Write-Host $path -NoNewline -ForegroundColor "blue"
+    Write-BranchName
+  } else {
+    Write-Host $path -ForegroundColor "blue"
+  }
+
+  return $userPrompt
+}
