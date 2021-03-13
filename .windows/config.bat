0bda679acc80fc3ee496c2b060ef8c1d78f71ed7
Lae Chen <laec@protonmail.ch>
2021-03-13 14:01:46 +1300

    Update settings.

--- .windows/config.bat
+++ .windows/config.bat
@@ -2,6 +2,7 @@ for /d %%p in ("C:\Users\Lae\AppData\Local\Packages\Microsoft.WindowsTerminal_*"
   mklink /D "%%~fp\LocalState" C:\Users\Lae\projects\self\dotfiles\.windows\terminal
 )
 
+mklink /D C:\Users\Lae\.azure C:\Users\Lae\projects\self\dotfiles\.azure
 mklink /D C:\Users\Lae\Documents\PowerShell C:\Users\Lae\projects\self\dotfiles\.windows\powershell
 mklink /D C:\Users\Lae\AppData\Roaming\Code\User C:\Users\Lae\projects\self\dotfiles\.config\Code\User
 mklink /J C:\Users\Lae\AppData\Roaming\JetBrains\IdeaIC2021.1 C:\Users\Lae\projects\self\dotfiles\.config\JetBrains\idea
